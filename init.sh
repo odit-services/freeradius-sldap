b0c3dcd044da28c7370e93f50b3cc6d8441b9111
Nicolai Ort <info@nicolai-ort.com>
2025-01-27 20:20:08 +0100

    tmp

--- init.sh
+++ init.sh
@@ -29,31 +29,31 @@ fi
 sed -i "s|DOMAIN_EXTENSION|$DOMAIN_EXTENSION|g" /etc/freeradius/mods-available/ldap
 
 # Handle the certs
-cp /certs/ldap-client.key /etc/freeradius/certs/ldap-client.key
-cp /certs/ldap-client.crt /etc/freeradius/certs/ldap-client.crt
-chown freerad:freerad /etc/freeradius/certs/ldap-client*
-chmod 640 /etc/freeradius/certs/ldap-client*
+cp /certs/* /etc/freeradius/certs/
+chown freerad:freerad /etc/freeradius/certs/*
+chmod 640 /etc/freeradius/certs/*
 
 # Handle the rest of the certificates
 # First the array of files which need 640 permissions
-FILES_640=( "ca.key" "server.key" "server.p12" "server.pem" "ldap-client.crt" "ldap-client.key" )
-for i in "${FILES_640[@]}"
-do
-	if [ -f "/certs/$i" ]; then
-	    cp /certs/$i /etc/raddb/certs/$i
-	    chmod 640 /etc/raddb/certs/$i
-	fi
-done
+# FILES_640=( "ca.key" "server.key" "server.p12" "server.pem" "ldap-client.crt" "ldap-client.key" )
+# for i in "${FILES_640[@]}"
+# do
+# 	if [ -f "/certs/$i" ]; then
+# 	    cp /certs/$i /etc/raddb/certs/$i
+# 	    chmod 640 /etc/raddb/certs/$i
+# 	    chmod 640 /etc/raddb/certs/$i
+# 	fi
+# done
 
-# Now all files that need a 644 permission set
-FILES_644=( "ca.pem" "server.crt" "server.csr" "dh" )
-for i in "${FILES_644[@]}"
-do
-	if [ -f "/certs/$i" ]; then
-	    cp /certs/$i /etc/freeradius/certs/$i
-	    chmod 644 /etc/freeradius/certs/$i
-	fi
-done
+# # Now all files that need a 644 permission set
+# FILES_644=( "ca.pem" "server.crt" "server.csr" "dh" )
+# for i in "${FILES_644[@]}"
+# do
+# 	if [ -f "/certs/$i" ]; then
+# 	    cp /certs/$i /etc/raddb/certs/$i
+# 	    chmod 644 /etc/raddb/certs/$i
+# 	fi
+# done
 
 
 /docker-entrypoint.sh "$@"
