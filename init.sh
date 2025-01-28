d5cee3d5946888c8e2f9cf4ca2fdeec2b8ea2732
Nicolai Ort <info@nicolai-ort.com>
2025-01-28 17:53:51 +0100

    refactor(init.sh): Remove commented-out certificate handling code

--- init.sh
+++ init.sh
@@ -33,27 +33,5 @@ cp /certs/* /etc/freeradius/certs/
 chown freerad:freerad /etc/freeradius/certs/*
 chmod 640 /etc/freeradius/certs/*
 
-# Handle the rest of the certificates
-# First the array of files which need 640 permissions
-# FILES_640=( "ca.key" "server.key" "server.p12" "server.pem" "ldap-client.crt" "ldap-client.key" )
-# for i in "${FILES_640[@]}"
-# do
-# 	if [ -f "/certs/$i" ]; then
-# 	    cp /certs/$i /etc/raddb/certs/$i
-# 	    chmod 640 /etc/raddb/certs/$i
-# 	    chmod 640 /etc/raddb/certs/$i
-# 	fi
-# done
-
-# # Now all files that need a 644 permission set
-# FILES_644=( "ca.pem" "server.crt" "server.csr" "dh" )
-# for i in "${FILES_644[@]}"
-# do
-# 	if [ -f "/certs/$i" ]; then
-# 	    cp /certs/$i /etc/raddb/certs/$i
-# 	    chmod 644 /etc/raddb/certs/$i
-# 	fi
-# done
-
 
 /docker-entrypoint.sh "$@"
